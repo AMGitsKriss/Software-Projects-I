13afcf53fb23d05b9e95a713ba7c00ee2b0c01b5
Dom Li <dli002@gold.ac.uk>
2016-04-29 14:15:54 +0100

    basic search code to impletment and adjust

---
+++ searchFunction/search.php
@@ -0,0 +1,20 @@
+<--Basic form action-->
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/hmtl; charset= utf-8" />
+        <link rel="stylesheet" media="all" href="css/style.css" />
+<title> Search Entries </title>
+</head>
+<body>
+<br>
+<br>
+
+<!-Basic form action->
+<form action="searchPage.php" method="GET">
+Search: <input type="text" name="title" placeholder=" Search here ... "/>
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html> 
+
